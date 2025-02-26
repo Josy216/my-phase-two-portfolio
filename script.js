4327f5fe7e4d8f20509848b16b2169a8913772ac
Joseph Teka <179788482+Josy216@users.noreply.github.com>
2025-02-26 14:15:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const btn = document.querySelector("#more");
 const morePro = document.querySelector(".more-pro");
-btn.addEventListener("click", function more() {
-  morePro.style.display = "flex";
+
+btn.addEventListener("click", function () {
+  if (morePro.style.display === "flex") {
+    morePro.style.display = "none";
+    btn.textContent = "Show More"; // Change text back to "Show More"
+  } else {
+    morePro.style.display = "flex";
+    btn.textContent = "Show Less"; // Change text to "Show Less"
+  }
 });
